a642422cda77ee09415a10b1f0e579ff82bd4153
Kaun Anantharman <karun11f29@gmail.com>
2021-10-14 18:49:45 +0530

    Base64 Final

--- Computer_Science/10_Cryptography/Vignere_Cipher/script.js
+++ Computer_Science/10_Cryptography/Vignere_Cipher/script.js
@@ -1,17 +1,20 @@
+function toBinary(string) {
+    const codeUnits = new Uint16Array(string.length);
+    for (let i = 0; i < codeUnits.length; i++) {
+      codeUnits[i] = string.charCodeAt(i);
+    }
+    const charCodes = new Uint8Array(codeUnits.buffer);
+    let result = '';
+    for (let i = 0; i < charCodes.byteLength; i++) {
+      result += String.fromCharCode(charCodes[i]);
+    }
+    return result;
+  }
+  
 function encrypt(){
     plain = document.forms["enc"]["plain"].value
-    key = document.forms["enc"]["key"].value
-    cipher = '';
-    for(var i=0,j=0;i<plain.length;i++){
-    var subt1=97,subt2=97;
-    var ch=plain.charCodeAt(i);
-    var k=key.charCodeAt(j%key.length);
-    if(ch>=65 && ch<=90) subt1=65;
-    if(k>=65 && k<=90) subt2=65; 
-    var c=(ch-subt1+k-subt2)%26+subt1;
-    if (ch >= 65 && ch <= 90 || ch >= 97 && ch <= 122) {cipher = cipher + String.fromCharCode(c);j++;}
-    else cipher += plain[i];
-    }
+    bstring = toBinary(plain)
+    cipher = btoa(bstring)
     console.log(cipher)
     document.getElementById('output-box').innerHTML = cipher
     document.getElementById('output-box').style.visibility = "visible"
@@ -19,18 +22,7 @@ function encrypt(){
 
 function decrypt(){
     cipher = document.forms["dec"]["cipher"].value
-    key = document.forms["dec"]["key"].value
-    plain = '';
-    for(var i=0,j=0;i<cipher.length;i++){
-    var subt1=97,subt2=97;
-    var ch=cipher.charCodeAt(i);
-    var k=key.charCodeAt(j%key.length);
-    if(ch>=65 && ch<=90) subt1=65;
-    if(k>=65 && k<=90) subt2=65; 
-    var c=(ch-subt1+26-(k-subt2))%26+subt1;
-    if (ch >= 65 && ch <= 90 || ch >= 97 && ch <= 122) {plain = plain + String.fromCharCode(c);j++}
-    else plain += cipher[i];
-    }
+    plain = atob(cipher)
     console.log(plain)
     document.getElementById('output-box').innerHTML = plain
     document.getElementById('output-box').style.visibility = "visible"
